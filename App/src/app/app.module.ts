b287368538c4f970e0aac2dc4208a40c4a321549
efren sandoval <efren.sandoval@epn.edu.ec>
2018-08-08 10:14:05 -0500

    Mostrando el componente pedidos en el AppComponent

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PaginaInicioComponent } from './pagina-inicio/pagina-inicio.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaInicioComponent,
+    PedidosComponent
   ],
   imports: [
     BrowserModule
