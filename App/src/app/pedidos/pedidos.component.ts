e8b5e7c55b3f92f7ed542667c1c597d2181ffbf2
efren sandoval <efren.sandoval@epn.edu.ec>
2018-08-19 11:18:26 -0500

    Vista pedidos solicitados terminado

--- App/src/app/pedidos/pedidos.component.ts
+++ App/src/app/pedidos/pedidos.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {PEDIDOS} from '../mock-pedidos';
+import {Pedido} from "../pedido";
 
 @Component({
   selector: 'app-pedidos',
@@ -9,6 +10,13 @@ import {PEDIDOS} from '../mock-pedidos';
 export class PedidosComponent implements OnInit {
 
   pedidos = PEDIDOS;
+
+  pedidoSeleccionado: Pedido;
+
+  onSelect(pedido: Pedido): void {
+    this.pedidoSeleccionado = pedido;
+    console.log(this.pedidoSeleccionado);
+  }
 
   constructor() {
   }
