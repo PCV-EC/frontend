6ea30b66c92897fdff5fcc6d98836864d8423775
efren sandoval <efren.sandoval@epn.edu.ec>
2018-08-24 16:47:31 -0500

    Componente mensajes creado

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { PaginaInicioComponent } from './pagina-inicio/pagina-inicio.component';
 import { PedidosComponent } from './pedidos/pedidos.component';
 import { DetallePedidoComponent } from './detalle-pedido/detalle-pedido.component';
+import { MensajesComponent } from './mensajes/mensajes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PaginaInicioComponent,
     PedidosComponent,
-    DetallePedidoComponent
+    DetallePedidoComponent,
+    MensajesComponent
   ],
   imports: [
     BrowserModule
