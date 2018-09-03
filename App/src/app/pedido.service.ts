23f70468432d5721ed85852f72f14672f4beae99
efren sandoval <efren.sandoval@epn.edu.ec>
2018-09-03 13:38:07 -0500

    Se inyecta MensajeServicio en Pedidos

--- App/src/app/pedido.service.ts
+++ App/src/app/pedido.service.ts
@@ -2,15 +2,22 @@ import { Injectable } from '@angular/core';
 import { Pedido} from "./pedido";
 import { PEDIDOS} from "./mock-pedidos";
 import { Observable, of } from "rxjs";
+import { MensajeService } from "./mensaje.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PedidoService {
 
+  constructor(private mensajeService: MensajeService) { }
+
   obtenerPedidos(): Observable<Pedido[]> {
+    this.mensajeService.add('PedidoService: pedidos ya son locales');
     return of(PEDIDOS);
   }
 
-  constructor() { }
+  obtenerPedido(id: number): Observable<Pedido>{
+    this.mensajeService.add(`PedidoService: pedido id=${id} capturado`);
+    return of(PEDIDOS.find(pedido => pedido.idPedido === id));
+  }
 }
