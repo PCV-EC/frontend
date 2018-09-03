00890e145b7913750c1b4e4adc761114ab09f4b0
efren sandoval <efren.sandoval@epn.edu.ec>
2018-09-03 13:46:23 -0500

    Enrutamiento del componente DetallePedido
    Se agragan las características de enrutamiento a la clase y se agrega un boton de navegación al template.

--- App/src/app/detalle-pedido/detalle-pedido.component.ts
+++ App/src/app/detalle-pedido/detalle-pedido.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
-import { Pedido } from "../pedido";
+import {Pedido} from "../pedido";
+import {ActivatedRoute} from "@angular/router";
+import {Location} from "@angular/common";
+import {PedidoService} from "../pedido.service";
 
 @Component({
   selector: 'app-detalle-pedido',
@@ -10,9 +13,24 @@ export class DetallePedidoComponent implements OnInit {
 
   @Input() pedido: Pedido;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private ruta: ActivatedRoute,
+    private pedidoService: PedidoService,
+    private location: Location
+  ) {
   }
 
+  ngOnInit(): void {
+    this.obtenerPedido();
+  }
+
+  obtenerPedido(): void {
+    const idPedido = +this.ruta.snapshot.paramMap.get('id');
+    this.pedidoService.obtenerPedido(idPedido)
+      .subscribe(pedido => this.pedido = pedido);
+  }
+
+  regresar(): void {
+    this.location.back();
+  }
 }
